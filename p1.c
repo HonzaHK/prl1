f9de336b69c816f3870681258cff0e92c5a5538d
HonzaHK <zgupie@gmail.com>
2017-04-05 21:08:27 +0200

    NEW: test script; UPDATE: reading input sequence from file

--- p1.c
+++ p1.c
@@ -1,29 +1,56 @@
-//FIT VUTBR - POS - project 1
-//JAN KUBIS / xkubis13
+//VUTBR - FIT - PRL - project1
+//Jan Kubis / xkubis13
 
+#include <mpi.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <omp.h>
 
-int main(){
+#define BUFSIZE 128
+#define TAG 0
+
+int main(int argc, char *argv[]){
+
+	int numprocs = 0; //pocet procesoru
+    int myid = 0; //muj rank
+
+    FILE *inFilePtr = fopen("numbers", "rb"); 
+    fseek(inFilePtr, 0, SEEK_END);
+    int filelen = ftell(inFilePtr);
+    rewind(inFilePtr);
+
+
+	unsigned char *nums; 
+	int numsCnt = filelen;
+
+	if ((nums = malloc(sizeof(unsigned char) * numsCnt)) == NULL) {
+		perror("Error allocating memory");
+		abort();
+	}
+	memset(nums, 0, sizeof(int)*numsCnt);
 	
-	int nthreads, tid;
-
-/* Fork a team of threads giving them their own copies of variables */
-	#pragma omp parallel private(nthreads, tid)
-	{
-
-		/* Obtain thread number */
-		tid = omp_get_thread_num();
-		printf("Hello World from thread = %d\n", tid);
-
-		/* Only master thread does this */
-		if (tid == 0) {
-			nthreads = omp_get_num_threads();
-			printf("Number of threads = %d\n", nthreads);
-		}
-
+	fread(nums, filelen, 1, inFilePtr); 
+	for(int i = 0; i < numsCnt; ++i) {
+		printf("%d ",nums[i]);
 	}
 
+
+
+
+
+
+	//MPI INIT
+	MPI_Init(&argc,&argv); // inicializace MPI 
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs); // zjistíme, kolik procesů běží 
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid); // zjistíme id svého procesu 
+
+	if(myid==0){
+		printf("MASTA");
+	}
+
+
+
+	MPI_Finalize(); 
+	printf("\n");
 	return 0;
 }
---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SOURCE="p1.c"
+EXECUTABLE="p1"
+FLAGS="-std=gnu99 -Wall -Wextra -pedantic"
+
+if [ -f $EXECUTABLE ]; then
+	rm $EXECUTABLE
+fi
+
+
+echo "COMPILATION ---------------------------------------"
+#preklad c zdrojaku
+mpicc $FLAGS $SOURCE -o $EXECUTABLE
+echo "---------------------------------------------------"
+
+if [ -f $EXECUTABLE ]; then
+	#pocet cisel bud zadam nebo 10 :)
+	if [ $# -lt 1 ];then 
+	    numbers=10;
+	else
+	    numbers=$1;
+	fi;
+	#vyrobeni souboru s random cisly
+	dd if=/dev/random bs=1 count=$numbers of=numbers
+	#spusteni
+	mpirun -np $numbers $EXECUTABLE
+	#./$EXECUTABLE
+fi
+
+#uklid
+if [ -f $EXECUTABLE ]; then
+	rm $EXECUTABLE numbers
+fi
